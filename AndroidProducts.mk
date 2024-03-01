152191f193ee4a45797afc8dae9931e626ad6916
UnmoveD <unmoved2121@gmail.com>
2024-03-01 18:43:25 +0100

    sweet: Welcome Calyxos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/calyx_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    calyx_sweet-user \
+    calyx_sweet-userdebug \
+    calyx_sweet-eng
--- calyx_sweet.mk
+++ calyx_sweet.mk
@@ -11,10 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CalyxOS stuff.
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+CALYXOS_MAINTAINER := UnmoveD
+
+PRODUCT_NAME := calyx_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
